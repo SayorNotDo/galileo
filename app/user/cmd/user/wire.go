68ba87d41ed97487d6ca920ebe125965ac39ee6e
sayornotdo <jcskye07@hotmail.com>
2022-12-30 14:44:53 +0800

    Initial commit

---
+++ app/user/cmd/user/wire.go
@@ -0,0 +1,23 @@
+//go:build wireinject
+// +build wireinject
+
+// The build tag makes sure the stub is not built in the final build.
+
+package main
+
+import (
+	"galileo/app/user/internal/biz"
+	"galileo/app/user/internal/conf"
+	"galileo/app/user/internal/data"
+	"galileo/app/user/internal/server"
+	"galileo/app/user/internal/service"
+
+	"github.com/go-kratos/kratos/v2"
+	"github.com/go-kratos/kratos/v2/log"
+	"github.com/google/wire"
+)
+
+// wireApp init kratos application.
+func wireApp(*conf.Server, *conf.Data, log.Logger) (*kratos.App, func(), error) {
+	panic(wire.Build(server.ProviderSet, data.ProviderSet, biz.ProviderSet, service.ProviderSet, newApp))
+}
