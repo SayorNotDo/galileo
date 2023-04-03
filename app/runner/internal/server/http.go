d85eda8d758f3719f92d808dfd2c47ed829c01d8
sayornotdo <jcskye07@hotmail.com>
2023-04-03 20:10:08 +0800

    add router for runner base on gin

--- app/runner/internal/server/http.go
+++ app/runner/internal/server/http.go
@@ -4,17 +4,17 @@ import (
 	"context"
 	"fmt"
 	"galileo/app/runner/internal/conf"
+	"galileo/app/runner/internal/router"
 	"galileo/app/runner/internal/service"
-	"github.com/gin-contrib/cors"
-	"github.com/gin-gonic/gin"
-	kgin "github.com/go-kratos/gin"
-	"github.com/go-kratos/kratos/v2/errors"
 	"github.com/go-kratos/kratos/v2/log"
 	"github.com/go-kratos/kratos/v2/middleware"
+	"github.com/go-kratos/kratos/v2/middleware/logging"
+	"github.com/go-kratos/kratos/v2/middleware/metrics"
 	"github.com/go-kratos/kratos/v2/middleware/recovery"
+	"github.com/go-kratos/kratos/v2/middleware/tracing"
+	"github.com/go-kratos/kratos/v2/middleware/validate"
 	"github.com/go-kratos/kratos/v2/transport"
 	"github.com/go-kratos/kratos/v2/transport/http"
-	"time"
 )
 
 func customMiddleware(handler middleware.Handler) middleware.Handler {
@@ -28,7 +28,15 @@ func customMiddleware(handler middleware.Handler) middleware.Handler {
 }
 
 func NewHTTPServer(c *conf.Server, runner *service.RunnerService, logger log.Logger) *http.Server {
-	var opts []http.ServerOption
+	var opts = []http.ServerOption{
+		http.Middleware(
+			recovery.Recovery(),
+			tracing.Server(),
+			logging.Server(logger),
+			metrics.Server(),
+			validate.Validator(),
+		),
+	}
 	if c.Http.Network != "" {
 		opts = append(opts, http.Network(c.Http.Network))
 	}
@@ -38,25 +46,7 @@ func NewHTTPServer(c *conf.Server, runner *service.RunnerService, logger log.Log
 	if c.Http.Timeout != nil {
 		opts = append(opts, http.Timeout(c.Http.Timeout.AsDuration()))
 	}
-	router := gin.Default()
-	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"*"},
-		AllowMethods:     []string{"POST", "GET", "OPTIONS", "DELETE", "PUT", "PATCH"},
-		AllowHeaders:     []string{"Authorization", "Content-Type", "Upgrade", "Origin", "Connection", "Accept-Encoding", "Accept-Language", "Host", "x-requested-with", "CurrentTeamID"},
-		ExposeHeaders:    []string{"Content-Length", "Access-Control-Allow-Origin", "Access-Control-Allow-Headers", "Content-Type"},
-		AllowCredentials: true,
-		MaxAge:           12 * time.Hour,
-	}))
-	router.Use(kgin.Middlewares(recovery.Recovery(), customMiddleware))
 	srv := http.NewServer(opts...)
-	srv.HandlePrefix("/", router)
-	router.GET("/helloworld/:name", func(ctx *gin.Context) {
-		name := ctx.Param("name")
-		if name == "error" {
-			kgin.Error(ctx, errors.Unauthorized("UNAUTHORIZED", "no authorization"))
-		} else {
-			ctx.JSON(200, map[string]string{"welcome": name})
-		}
-	})
+	srv.HandlePrefix("/", router.RegisterHTTPServer(runner))
 	return srv
 }
