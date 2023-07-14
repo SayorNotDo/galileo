b772694187608c6634855013913465f321935329
sayornotdo <jcskye07@hotmail.com>
2023-07-14 13:31:48 +0800

    cron module dev

---
+++ app/cron/cmd/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"github.com/robfig/cron/v3"
+	"time"
+)
+
+func main() {
+	c := cron.New()
+
+	c.AddFunc("@every 1s", func() {
+		fmt.Println("tick every 1 second")
+	})
+
+	c.Start()
+	time.Sleep(time.Second * 5)
+}
